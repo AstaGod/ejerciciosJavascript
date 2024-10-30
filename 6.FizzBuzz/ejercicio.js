da5d6eff6e99ecbbade6b99839f2fca9ecb49c58
AstaGod <bautistapomacrhistianalexis@gmail.com>
2024-10-30 12:43:20 -0500

    feat:solucion del ejercicio 6

---
+++ 6.FizzBuzz/ejercicio.js
@@ -0,0 +1,18 @@
+// escribe tu respuesta acá
+function fizzBuzz(numero) {
+    if (numero % 3 === 0 && numero % 5 === 0) {
+        return "fizzbuzz";
+    }
+    if (numero % 3 === 0) {
+        return "fizz";
+    }
+    if (numero % 5 === 0) {
+        return "buzz"; 
+    }
+    return numero + ""; 
+}
+// código de prueba
+console.log(fizzBuzz(6)); // "fizz"
+console.log(fizzBuzz(20)); // "buzz"
+console.log(fizzBuzz(30)); // "fizzbuzz"
+console.log(fizzBuzz(8)); // 8
